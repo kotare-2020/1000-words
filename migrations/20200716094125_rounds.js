a4c1d3049511a8e82895ff63c60e00a30d7c9e34
JaydenHall <jaydenhallpt@hotmail.co.nz>
2020-07-16 17:29:56 +1200

    stuck on final add round data

--- migrations/20200716094125_rounds.js
+++ migrations/20200716094125_rounds.js
@@ -2,16 +2,16 @@
 exports.up = function(knex) {
   return knex.schema.createTable('rounds', (table) => {
       table.integer('player')
-      table.string('write_1')
-      table.string('draw_1')
-      table.string('write_2')
-      table.string('draw_2')
-      table.string('write_3')
-      table.string('draw_3')
-      table.string('write_4')
-      table.string('draw_4')
-      table.string('write_5')
-      table.string('draw_5')
+      table.string('round1')
+      table.string('round2')
+      table.string('round3')
+      table.string('round4')
+      table.string('round5')
+      table.string('round6')
+      table.string('round7')
+      table.string('round8')
+      table.string('round9')
+      table.string('round10')
   })
 };
 

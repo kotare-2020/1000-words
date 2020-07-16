a8313de3766988e7a710bfb33f7f3d020d9b2fda
andrewtempany <andrewtempany95@gmail.com>
2020-07-16 14:27:12 +1200

    create ViewSpace Component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Canvas from './canvas'
+import ViewSpace from './view'
 
 const App = () => {
   return (<>
     <h1>Time to Draw!</h1>
+    <div id="container"></div>
     <Canvas/>
+    <ViewSpace/>
+
     </>
   )
 }
--- client/components/canvas.jsx
+++ client/components/canvas.jsx
@@ -5,77 +5,90 @@ import React from 'react'
 class Canvas extends React.Component {
 
 
-    
 
-    componentDidMount(){
+    // stage = null
+
+
+    componentDidMount() {
         var select = document.getElementById('tool');
         console.log(select)
+        select.addEventListener('change', function () {
+            mode = select.value;
+        });
+        var width = window.innerWidth ;
+        var height = window.innerHeight - 25;
+        // first we need Konva core things: stage and layer
+       
+       //stage is our drawing space
+        this.stage = new Konva.Stage({
+            container: 'container',
+            width: width,
+            height: height,
+        })
+    
+        //layer is our drawing layer on top of stage
+        var layer = new Konva.Layer();
+        this.stage.add(layer);
+    
+        var isPaint = false;
+        var mode = 'brush';
+        var lastLine;
+    
+        this.stage.on('mousedown touchstart', (e) => {
+            isPaint = true;
+            var pos = this.stage.getPointerPosition();
+            lastLine = new Konva.Line({
+                stroke: '#DF4B26',
+                strokeWidth: 5,
+                globalCompositeOperation:
+                    mode === 'brush' ? 'source-over' : 'destination-out',
+                points: [pos.x, pos.y],
+            });
+            layer.add(lastLine);
+        });
+    
+        this.stage.on('mouseup touchend', () => {
+            isPaint = false;
+            console.log(this.stage.toJSON())
+        });
+    
+        // and core function - drawing
+        this.stage.on('mousemove touchmove',  () => {
+            if (!isPaint) {
+                return;
+            }
+            const pos = this.stage.getPointerPosition();
+            var newPoints = lastLine.points().concat([pos.x, pos.y]);
+            lastLine.points(newPoints);
+            layer.batchDraw();
+        });
     }
     
-    // select.addEventListener('change', function () {
-    //     mode = select.value;
-    // });
-
-
-    render() {
-        
-        var width = window.innerWidth;
-    var height = window.innerHeight - 25;
-    // first we need Konva core things: stage and layer
-    var stage = new Konva.Stage({
-        container: 'container',
-        width: width,
-        height: height,
-    })
-
-    var layer = new Konva.Layer();
-    stage.add(layer);
-
-    var isPaint = false;
-    var mode = 'brush';
-    var lastLine;
-
-    stage.on('mousedown touchstart', function (e) {
-        isPaint = true;
-        console.log(stage.toJSON())
-        var pos = stage.getPointerPosition();
-        lastLine = new Konva.Line({
-            stroke: '#DF4B26',
-            strokeWidth: 5,
-            globalCompositeOperation:
-                mode === 'brush' ? 'source-over' : 'destination-out',
-            points: [pos.x, pos.y],
-        });
-        layer.add(lastLine);
-    });
-
-    stage.on('mouseup touchend', function () {
-        isPaint = false;
-    });
-
-    // and core function - drawing
-    stage.on('mousemove touchmove', function () {
-        if (!isPaint) {
-            return;
-        }
-        const pos = stage.getPointerPosition();
-        var newPoints = lastLine.points().concat([pos.x, pos.y]);
-        lastLine.points(newPoints);
-        layer.batchDraw();
-    });
     
-    return (
-        <>
+    
+    render() {
 
-            Tool:
-            <select id="tool">
-                <option value="brush">Brush</option>
-                <option value="eraser">Eraser</option>
-            </select>
-            {/* <div id="container"></div> */}
 
-        </>
-    )
+        return (
+            <>
+
+                Tool:
+                <select id="tool">
+                    <option value="brush">Brush</option>
+                    <option value="eraser">Eraser</option>
+                </select>
+                <button
+                    onClick={() => {
+                        localStorage.setItem(
+                            "drawing", this.stage.toJSON()
+                        );
+                    }}
+                >
+                    Save
+          </button>
+          <div id="container"></div>
+            </>
+        )
     }
 }
 

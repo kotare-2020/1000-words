de4b0bc54575b38be34a6813f4dd8629f7af8f9c
rubin-gower <rubinsemails@gmail.com>
2020-07-17 15:14:27 +1200

    socket connects on app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,24 +8,42 @@ import PlayerLobby from "./PlayerLobby"
 import drawcanvas from "./canvas"
 import HostLobby from "./HostLobby"
 
-const App = () => {
+class App extends React.Component {
 
-  return (
-  <>
+  componentDidMount(){
+    socket.on('connect', () => {
+
+      console.log("connected to websocket server")
+
+      socket.on("disconnect", () => {
+        console.log("connection to websocket server was lost")
+      })
+      
+    });
+  }
   
-    <Router>
-    dev menu <Link to="/">home</Link> | <Link to="/lobby">lobby</Link> | <Link to="/canvas">canvas</Link>
-          <Route exact path="/" component={HostJoin}/>
+
+  render() {
+
+
+    return (
+
+      <>
+
+        <Router>
+          dev menu <Link to="/">home</Link> | <Link to="/lobby">lobby</Link> | <Link to="/canvas">canvas</Link>
+          <Route exact path="/" component={HostJoin} />
           <Route exact path="/create" component={CreateGame} />
           <Route exact path="/join" component={JoinGame} />
           <Route exact path="/lobby" component={PlayerLobby} />
           <Route exact path="/canvas" component={drawcanvas} />
           <Route exact path="/createlobby" component={HostLobby} />
           {/* <Route exact path="/apis" component={APIs} /> */}
-  
-    </Router>
-    </>
-  )
+
+        </Router>
+      </>
+    )
+  }
 }
 
 export default App
--- client/components/JoinGame.jsx
+++ client/components/JoinGame.jsx
@@ -3,55 +3,68 @@ import { getGameIdApi, getPlayersInlobby, addPlayerApi } from "../apis/apis";
 import { HashRouter as Router, Route, Link, Redirect } from "react-router-dom";
 
 class JoinGame extends React.Component {
-  state = {
-    realgame: false,
-    hasRoom: false,
-  };
+    state = {
 
-  validategame = () => {
-    console.log(document.getElementById("gameid").value);
-    if (document.getElementById("gameid").value !== "") {
-      getGameIdApi(document.getElementById("gameid").value)
-        .then((res) => {
-          console.log(res.body);
-          if (res.body.game) {
-            console.log("game exists");
-            this.setState({ realgame: true });
-            getPlayersInlobby(document.getElementById("gameid").value).then(
-              (data) => {
-                console.log(data.body.length);
-                if (data.body.length < 10) {
-                  addPlayerApi({
-                    player_name: document.getElementById("name").value,
-                    game_id: document.getElementById("gameid").value,
-                    color: "black",
-                  });
-                }else alert("Lobby is full")
-              }
-            );
-          } else alert("that game dose not exist");
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+
+    };
+
+    validategame = () => {
+
+
+        getGameIdApi(document.getElementById("gameid").value)
+            .then((res) => {
+
+
+                if (res.body.game) {
+                    getPlayersInlobby(document.getElementById("gameid").value)
+                        .then((data) => {
+
+
+                            if (data.body.length < 10) {
+                                addPlayerApi(
+                                    {
+                                        player_name: document.getElementById("name").value,
+                                        game_id: document.getElementById("gameid").value,
+                                        color: "black",
+                                    }
+                                     
+                                )
+                                .then(() => {
+                                    console.log("can switch to next stage")
+                                document.getElementById("next").click()
+                                })
+                                .catch(err => {
+                                    console.log(err)
+                                })
+                            }
+                            else alert("Lobby is full")
+
+                        })
+
+                }
+                else alert("that game dose not exist");
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+
+    };
+    render() {
+        return (
+            <>
+                <div>
+                    <label>game id</label>
+                    <input id="gameid" type="text" name="lobby" placeholder="Game id" />
+                </div>
+                <div>
+                    <label>User Name</label>
+                    <input id="name" type="text" name="player" placeholder="Name" />
+                </div>
+                <button onClick={this.validategame}>Join</button>
+                <Link to="/lobby" ><div id="next" style={{display: "none"}}>to lobby</div></Link>
+            </>
+        );
     }
-  };
-  render() {
-    return (
-      <>
-        <div>
-          <label>game id</label>
-          <input id="gameid" type="text" name="lobby" placeholder="Game id" />
-        </div>
-        <div>
-          <label>User Name</label>
-          <input id="name" type="text" name="player" placeholder="Name" />
-        </div>
-        <button onClick={this.validategame}>Join</button>
-        <Link to="/lobby">to lobby</Link>
-      </>
-    );
-  }
 }
 
 export default JoinGame;

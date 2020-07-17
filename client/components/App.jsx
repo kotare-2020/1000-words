d7505e825cb36593148d649f0dc524444f90d47b
andrewtempany <andrewtempany95@gmail.com>
2020-07-17 16:36:52 +1200

    clean up imports in app.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -18,24 +18,24 @@ const App = () => {
 
 
   return (
-    
+
     <>
+      <Router>
 
-    //     <h1>Time to Draw!</h1>
-    //  <Writing JSON="this"/>
-    //  <Drawing/>
+        <h1>Time to Draw!</h1>
+        <Writing JSON="this" />
+        <Drawing />
 
-      {/* <Router>
-        dev menu <Link to="/">home</Link> | <Link to="/lobby">lobby</Link> | <Link to="/canvas">canvas</Link>
+        <Link to="/">home</Link> | <Link to="/lobby">lobby</Link> | <Link to="/canvas">canvas</Link>
         <Route exact path="/" component={HostJoin} />
         <Route exact path="/create" component={CreateGame} />
         <Route exact path="/join" component={JoinGame} />
         <Route exact path="/lobby" component={PlayerLobby} />
         <Route exact path="/canvas" component={drawcanvas} />
         <Route exact path="/createlobby" component={HostLobby} />
-        {/* <Route exact path="/apis" component={APIs} /> 
+        {/* <Route exact path="/apis" component={APIs} />  */}
 
-      </Router> */}
+      </Router>
     </>
   )
 }

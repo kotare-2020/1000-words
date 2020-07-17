9e574f0b3aabb638e2f7c672a14711dc10b77c0a
rubin-gower <rubinsemails@gmail.com>
2020-07-17 13:35:51 +1200

    added draw and admin lobby

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,19 +4,23 @@ import { HashRouter as Router, Route, Link, Redirect } from "react-router-dom"
 import HostJoin from "./HostJoin"
 import CreateGame from "./CreateGame"
 import JoinGame from "./JoinGame"
-import Lobby from "./PlayerLobby"
+import PlayerLobby from "./PlayerLobby"
 import drawcanvas from "./canvas"
+import HostLobby from "./HostLobby"
+
 const App = () => {
 
   return (
   <>
   
     <Router>
-    dev menu <Link to="/">home</Link> | <Link to="/lobby">lobby</Link> | <Link to="/drawcanvas">canvas</Link>
+    dev menu <Link to="/">home</Link> | <Link to="/lobby">lobby</Link> | <Link to="/canvas">canvas</Link>
           <Route exact path="/" component={HostJoin}/>
           <Route exact path="/create" component={CreateGame} />
           <Route exact path="/join" component={JoinGame} />
-          <Route exact path="/lobby" component={Lobby} />
+          <Route exact path="/lobby" component={PlayerLobby} />
+          <Route exact path="/canvas" component={drawcanvas} />
+          <Route exact path="/createlobby" component={HostLobby} />
           {/* <Route exact path="/apis" component={APIs} /> */}
   
     </Router>
